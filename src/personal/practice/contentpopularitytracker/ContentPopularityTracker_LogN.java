1097da67be17356964e4c6e209fe8fd088a68f17
jitendrasamal <jitendra_svnit@yahoo.co.in>
2025-02-16 21:53:38 +0530

    commit

---
+++ src/personal/practice/contentpopularitytracker/ContentPopularityTracker_LogN.java
@@ -0,0 +1,83 @@
+package personal.practice.contentpopularitytracker;
+
+import javafx.collections.transformation.SortedList;
+
+import java.util.*;
+
+public class ContentPopularityTracker_LogN {
+
+    SortedSet<Node> sortedByPopularity = new TreeSet<>((n1, n2) -> {
+        if (n1.popularityCount == n2.popularityCount)
+            return n1.contentId - n2.contentId;
+        return n1.popularityCount - n2.popularityCount;
+    });
+    Map<Integer, Integer> contentIdToCountHashMap = new HashMap<>();
+
+
+    public static void main(String[] args) {
+        ContentPopularityTracker_LogN tracker = new ContentPopularityTracker_LogN();
+
+        // Basic Test Cases
+        tracker.increasePopularity(1);
+        tracker.increasePopularity(2);
+        tracker.increasePopularity(2);
+        tracker.decreasePopularity(1);
+        System.out.println("Most Popular Content ID: " + tracker.mostPopular()); // Expected Output: 2
+
+        // Edge Case: Multiple increases and decreases
+        tracker.increasePopularity(3);
+        tracker.increasePopularity(3);
+        tracker.increasePopularity(3);
+        //tracker.print();
+        tracker.decreasePopularity(2);
+        //tracker.print();
+        System.out.println("Most Popular Content ID: " + tracker.mostPopular()); // Expected Output: 3
+
+        // Edge Case: Content getting removed
+        tracker.decreasePopularity(3);
+        tracker.decreasePopularity(3);
+        tracker.decreasePopularity(3);
+        //tracker.print();
+        System.out.println("Most Popular Content ID: " + tracker.mostPopular()); // Expected Output: 2
+
+        // Edge Case: No content
+        tracker.decreasePopularity(2);
+        System.out.println("Most Popular Content ID: " + tracker.mostPopular()); // Expected Output: -1
+    }
+
+    public void increasePopularity(int contentId) {
+        int count = contentIdToCountHashMap.getOrDefault(contentId, 0);
+        if (count > 0) {
+            sortedByPopularity.remove(new Node(contentId, count));
+        }
+        contentIdToCountHashMap.put(contentId, count + 1);
+        sortedByPopularity.add(new Node(contentId, count + 1));
+    }
+
+    public void decreasePopularity(int contentId) {
+        if (!contentIdToCountHashMap.containsKey(contentId))
+            return;
+        int count = contentIdToCountHashMap.get(contentId);
+        contentIdToCountHashMap.remove(contentId);
+        sortedByPopularity.remove(new Node(contentId, count));
+        if (count > 1) {
+            contentIdToCountHashMap.put(contentId, count - 1);
+            sortedByPopularity.add(new Node(contentId, count - 1));
+        }
+
+    }
+
+    public int mostPopular() {
+        return sortedByPopularity.isEmpty() ? -1 : sortedByPopularity.last().contentId;
+    }
+
+    private static class Node {
+        int popularityCount;
+        int contentId;
+
+        public Node(int contentId, int popularityCount) {
+            this.contentId = contentId;
+            this.popularityCount = popularityCount;
+        }
+    }
+}
---
+++ src/personal/practice/contentpopularitytracker/ContentPopularityTracker_LogN_part2.java
@@ -0,0 +1,80 @@
+package personal.practice.contentpopularitytracker;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+
+public class ContentPopularityTracker_LogN_part2 {
+
+    private final Map<Integer, Integer> contentIdToCount;
+    private final TreeMap<Integer, Set<Integer>> countToContentId;
+    public ContentPopularityTracker_LogN_part2() {
+        this.contentIdToCount = new HashMap<>();
+        this.countToContentId = new TreeMap<>();
+    }
+
+    public static void main(String[] args) {
+        ContentPopularityTracker_LogN_part2 tracker = new ContentPopularityTracker_LogN_part2();
+
+        // Basic Test Cases
+        tracker.increasePopularity(1);
+        tracker.increasePopularity(2);
+        tracker.increasePopularity(2);
+        tracker.decreasePopularity(1);
+        System.out.println("Most Popular Content ID: " + tracker.mostPopular()); // Expected Output: 2
+
+        // Edge Case: Multiple increases and decreases
+        tracker.increasePopularity(3);
+        tracker.increasePopularity(3);
+        tracker.increasePopularity(3);
+        //tracker.print();
+        tracker.decreasePopularity(2);
+        //tracker.print();
+        System.out.println("Most Popular Content ID: " + tracker.mostPopular()); // Expected Output: 3
+
+        // Edge Case: Content getting removed
+        tracker.decreasePopularity(3);
+        tracker.decreasePopularity(3);
+        tracker.decreasePopularity(3);
+        //tracker.print();
+        System.out.println("Most Popular Content ID: " + tracker.mostPopular()); // Expected Output: 2
+
+        // Edge Case: No content
+        tracker.decreasePopularity(2);
+        System.out.println("Most Popular Content ID: " + tracker.mostPopular()); // Expected Output: -1
+    }
+
+    public void increasePopularity(final Integer contentId) {
+        impression(contentId, true);
+    }
+
+    public void decreasePopularity(final Integer contentId) {
+        impression(contentId, false);
+    }
+
+    public int mostPopular() {
+        if (countToContentId.isEmpty()) {
+            return -1;
+        }
+        return countToContentId.lastEntry().getValue().stream().findFirst().get();
+    }
+
+    private void impression(final Integer contentId, boolean increase) {
+        int val = increase ? 1 : -1;
+        if (!contentIdToCount.containsKey(contentId)) {
+            contentIdToCount.put(contentId, val);
+            countToContentId.computeIfAbsent(val, k -> new HashSet<>()).add(contentId);
+        } else {
+            int count = contentIdToCount.get(contentId);
+            contentIdToCount.merge(contentId, val, Integer::sum);
+            countToContentId.get(count).remove(contentId);
+            if (countToContentId.get(count).isEmpty()) {
+                countToContentId.remove(count);
+            }
+            countToContentId.computeIfAbsent(count + val, k -> new HashSet<>()).add(contentId);
+        }
+    }
+
+}
