de02c335f7b58412bc218e9ea0b504322bd7eaa3
jitendrasamal <jitendra_svnit@yahoo.co.in>
2025-02-20 20:05:01 +0530

    commit

--- src/personal/practice/newrelic/PolishNotation.java
+++ src/personal/practice/newrelic/PolishNotation.java
@@ -9,6 +9,59 @@ public class PolishNotation {
         PolishNotation polishNotation = new PolishNotation();
         String exp = "a+b*(c^d-e)^(f+g*h)-i";
         polishNotation.infixToPostfix(exp);
+        polishNotation.infixToPrefix(exp);
+    }
+
+    void infixToPrefix(String s) {
+        Stack<Character> operators = new Stack<>();
+        Stack<String> operands = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+
+            // If the character is an operand, push it to the operands stack
+            if (Character.isLetterOrDigit(c)) {
+                operands.push(c + "");
+            }
+            // If the character is '(', push it to the operators stack
+            else if (c == '(') {
+                operators.push(c);
+            }
+            // If the character is ')', pop from both stacks and push to the operands stack until '(' is found
+            else if (c == ')') {
+                while (!operators.isEmpty() && operators.peek() != '(') {
+                    String op1 = operands.pop();
+                    String op2 = operands.pop();
+                    char op = operators.pop();
+                    String exp = op + op2 + op1;
+                    operands.push(exp);
+                }
+                operators.pop();
+            }
+            // If an operator is found
+            else {
+                while (!operators.isEmpty() && precedence(c) <= precedence(operators.peek())) {
+                    String op1 = operands.pop();
+                    String op2 = operands.pop();
+                    char op = operators.pop();
+                    String exp = op + op2 + op1;
+                    operands.push(exp);
+                }
+                operators.push(c);
+            }
+        }
+
+        // Pop all the remaining operators from the stack
+        while (!operators.isEmpty()) {
+            String op1 = operands.pop();
+            String op2 = operands.pop();
+            char op = operators.pop();
+            String exp = op + op2 + op1;
+            operands.push(exp);
+        }
+
+        // The final prefix expression is in the operands stack
+        System.out.println(operands.pop());
     }
 
     // Function to perform infix to postfix conversion
---
+++ src/personal/practice/newrelic/SpiralMatrix.java
@@ -0,0 +1,92 @@
+package personal.practice.newrelic;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+class SpiralMatrix {
+    public List<Integer> spiralOrder(int[][] matrix) {
+        List<Integer> result = new ArrayList<>();
+        int rows = matrix.length;
+        int columns = matrix[0].length;
+        int up = 0;
+        int left = 0;
+        int right = columns - 1;
+        int down = rows - 1;
+
+        while (result.size() < rows * columns) {
+            // Traverse from left to right.
+            for (int col = left; col <= right; col++) {
+                result.add(matrix[up][col]);
+            }
+            // Traverse downwards.
+            for (int row = up + 1; row <= down; row++) {
+                result.add(matrix[row][right]);
+            }
+            // Make sure we are now on a different row.
+            if (up != down) {
+                // Traverse from right to left.
+                for (int col = right - 1; col >= left; col--) {
+                    result.add(matrix[down][col]);
+                }
+            }
+            // Make sure we are now on a different column.
+            if (left != right) {
+                // Traverse upwards.
+                for (int row = down - 1; row > up; row--) {
+                    result.add(matrix[row][left]);
+                }
+            }
+            left++;
+            right--;
+            up++;
+            down--;
+        }
+
+        return result;
+    }
+
+    public List<Integer> spiralOrder_VISITED(int[][] matrix) {
+        int VISITED = 101;
+        int rows = matrix.length;
+        int columns = matrix[0].length;
+        // Four directions that we will move: right, down, left, up.
+        int[][] directions = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+        // Initial direction: moving right.
+        int currentDirection = 0;
+        // The number of times we change the direction.
+        int changeDirection = 0;
+        // Current place that we are at is (row, col).
+        // row is the row index; col is the column index.
+        int row = 0;
+        int col = 0;
+        // Store the first element and mark it as visited.
+        List<Integer> result = new ArrayList<>();
+        result.add(matrix[0][0]);
+        matrix[0][0] = VISITED;
+        while (changeDirection < 2) {
+            while (
+                    row + directions[currentDirection][0] >= 0 &&
+                            row + directions[currentDirection][0] < rows &&
+                            col + directions[currentDirection][1] >= 0 &&
+                            col + directions[currentDirection][1] < columns &&
+                            matrix[row + directions[currentDirection][0]][col +
+                                    directions[currentDirection][1]] !=
+                                    VISITED
+            ) {
+                // Reset this to 0 since we did not break and change the direction.
+                changeDirection = 0;
+                // Calculate the next place that we will move to.
+                row = row + directions[currentDirection][0];
+                col = col + directions[currentDirection][1];
+                result.add(matrix[row][col]);
+                matrix[row][col] = VISITED;
+            }
+            // Change our direction.
+            currentDirection = (currentDirection + 1) % 4;
+            // Increment change_direction because we changed our direction.
+            changeDirection++;
+        }
+        return result;
+    }
+}
