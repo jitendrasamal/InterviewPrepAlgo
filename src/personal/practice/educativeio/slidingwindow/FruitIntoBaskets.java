a0dbd6aa09d24b44028516380f295a29549e94cf
jitendrasamal <jitendra_svnit@yahoo.co.in>
2025-03-01 21:07:14 +0530

    commit

--- src/personal/practice/educativeio/slidingwindow/FruitIntoBaskets.java
+++ src/personal/practice/educativeio/slidingwindow/FruitIntoBaskets.java
@@ -14,27 +14,27 @@ public class FruitIntoBaskets {
     }
 
     public int totalFruit(int[] fruits) {
-        int maxFruit = 0;
-        int maxTypeOfFruit = 2;
+        int collected = 0;
+        int basketSize = 2;
         if (fruits.length == 0)
-            return maxFruit;
-        HashMap<Integer, Integer> fruitTypeToCount = new HashMap<>();
+            return collected;
+        HashMap<Integer, Integer> baskets = new HashMap<>();
         int start = 0;
         for (int end = 0; end < fruits.length; ++end) {
             int type = fruits[end];
-            fruitTypeToCount.put(type, fruitTypeToCount.getOrDefault(type, 0) + 1);
-            while (fruitTypeToCount.size() > maxTypeOfFruit) {
+            baskets.put(type, baskets.getOrDefault(type, 0) + 1);
+            while (baskets.size() > basketSize) {
                 type = fruits[start];
-                int count = fruitTypeToCount.get(type) - 1;
+                int count = baskets.get(type) - 1;
                 if (count > 0) {
-                    fruitTypeToCount.put(type, count);
+                    baskets.put(type, count);
                 } else {
-                    fruitTypeToCount.remove(type);
+                    baskets.remove(type);
                 }
                 ++start;
             }
-            maxFruit = Math.max(maxFruit, end - start + 1);
+            collected = Math.max(collected, end - start + 1);
         }
-        return maxFruit;
+        return collected;
     }
 }
